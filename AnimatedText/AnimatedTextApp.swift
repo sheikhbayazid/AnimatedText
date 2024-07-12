12d6d2cc7bd75485cf5a27e1b53729ed9797d353
Sheikh Bayazid <sheikhbayazid@gmail.com>
2024-07-12 13:48:39 +0200

    Initial Commit

---
+++ AnimatedText/AnimatedTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedTextApp.swift
+//  AnimatedText
+//
+//  Created by Sheikh Bayazid on 2024-07-12.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
